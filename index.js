2e360b412cff4b524194e7fe014d270e4b76e3df
Car Bgmi <127945393+Popeye68@users.noreply.github.com>
2023-10-01 03:42:56 +0000

    Update 2 files

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://3p6kv6-5000.csb.app";
 //TOGGLE for 1pt Proxy and Shorters
 var use1pt=true;
 
